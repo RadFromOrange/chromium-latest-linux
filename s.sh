a729a718bfb95bdb34c127704fadcf751c09c57d
RadFromOrange <radoinidrissi@hotmail.fr>
2024-10-19 20:06:47 +0000

    snake game

---
+++ s.sh
@@ -0,0 +1,12 @@
+sudo apt-get install ./kasmvncserver_*.deb
+
+# Add your user to the ssl-cert group
+sudo addgroup $USER ssl-cert
+
+# YOU MUST DISCONNECT AND RECONNECT FOR GROUP MEMBERSHIP CHANGE TO APPLY
+
+# start KasmVNC, you will be prompted to create a KasmVNC user and select a desktop environment
+vncserver
+
+# Tail the logs
+tail -f ~/.vnc/*.log
